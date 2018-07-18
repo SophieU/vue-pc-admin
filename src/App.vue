b6612d4fa902679f7fb93caa58501b06ebdd9c92
SophieU <j.u.sophia@qq.com>
2018-07-18 14:16:25 +0800

    finished the login page and add validate to the form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
@@ -11,13 +10,122 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  html,body{
+    padding: 0;
+    margin: 0;
+  }
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 0;
+  margin: 0;
+  font-family: "Helvetica Neue,Helvetica,Hiragino Sans GB,Microsoft YaHei";
+}
+  .text-right{
+    text-align:right;
+  }
+  /*transition定义动画*/
+  /* 设置持续时间和动画函数 */
+  .slide-fade-enter-active {
+    transition: all .3s ease;
+
+  }
+  .slide-fade-leave-active {
+    transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    opacity: 0;
+  }
+  .slide-fade-enter, .slide-fade-leave
+    /* .slide-fade-leave-active for below version 2.1.8 */ {
+    transform: translateX(30px);
+    opacity: 0;
+  }
+  .slide-enter-active{
+    animation: slideLeft .3s linear;
+  }
+  .slide-leave-active{
+    animation:slideRight .3s linear;
+  }
+  .slide-leave,.slide-enter{
+    opacity: 0;
+    transform:translateX(30px)
+  }
+  @keyframes slideLeft {
+    0%{
+      transform:translateX(30px);
+      opacity: 0;
+    }
+    100%{
+      transform:translateX(0);
+      opacity: 1;
+    }
+  }
+  @keyframes slideRight {
+    0%{
+      transform:translateX(0px);
+      opacity: 1;
+    }
+    100%{
+      transform:translateX(30px);
+      opacity: 0;
+    }
+  }
+
+
+  /*登录页对iview的修改*/
+  .ivu-btn-primary{
+    color: #fff;
+    background-color: #039dff;
+    border-color: #039dff;
+  }
+.login-container{
+  .ivu-form-item-required .ivu-form-item-label:before{
+    content: '';
+    display: none;
+  }
+  .ivu-form{
+    .ivu-btn{
+      &:focus{
+        box-shadow: none;
+      }
+    }
+    .ivu-form-item-label{
+      font-size: 14px!important;
+      padding-bottom: 8px;
+      color: #aeaeae;
+    }
+    .ivu-input{
+      border: none;
+      border-bottom: 1px solid #ccc;
+      border-radius: 0;
+      &:focus{
+        border-color: #039dff;
+        box-shadow: none;
+      }
+    }
+    .ivu-input-group-prepend{
+      color: #039dff;
+      text-align: center;
+      font-size: 18px;
+      background-color: transparent;
+      border: none;
+      border-bottom:1px solid #ccc;
+      border-radius: 0;
+    }
+    .ivu-input-group-append{
+      background-color: transparent;
+      border: none;
+      padding-left: 4px;
+      .ivu-btn{
+        border-color: #039dff;
+        background-color: #039dff;
+        color: #fff;
+        margin: -6px -7px;
+        margin-left: 5px;
+        outline:none;
+        &:active{
+          background: #2b85e4;
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ src/pages/login/Login.vue
@@ -0,0 +1,270 @@
+<template>
+    <div class="login-page">
+      <div class="login-container">
+        <div class="login-left">
+          <img src="./logo03.png" alt="">
+          <h4>物业管理后台</h4>
+        </div>
+        <div class="login-right">
+          <transition name="fade">
+
+            <div class="login-box" v-if="now==='login'||now==='register'">
+              <Form ref="loginForm" :model="formLogin" :rules="ruleLogin">
+                <FormItem label="Telephone" prop="username">
+                  <!--<Icon type="person"></Icon>-->
+                  <Input placeholder="请输入账户信息"   :maxlength="11"  type="text" v-model="formLogin.username">
+                    <span slot="prepend" class="login-ico"> <Icon type="person"></Icon></span>
+                  </Input>
+                </FormItem>
+                <FormItem label="Password" prop="password">
+                  <Input placeholder="请输入密码" type="password" v-model="formLogin.password">
+                    <span slot="prepend" class="login-ico"><Icon type="ios-locked"></Icon></span>
+                  </Input>
+                </FormItem>
+                <transition name="slide-fade">
+                  <FormItem label="Code"  v-show="now==='register'" prop="authCode">
+                    <Input placeholder="请输入密码" type="password" v-model="formLogin.password">
+                    <span slot="prepend" class="login-ico"><Icon type="outlet"></Icon></span>
+                    <span slot="append" class="code-btn">
+                    <Button  type="primary" :loading="getCoding" @click="getCodingHandle">验证码</Button>
+                  </span>
+                    </Input>
+                  </FormItem>
+                </transition>
+
+                <FormItem>
+                  <Button type="primary" long @click="validateLogin">登录</Button>
+                  <Row>
+                    <Col span="12" offset="12" class-name="text-right">
+                      <Button type="text" @click="togglePanel('forget')">忘记密码？</Button>
+                    </Col>
+                  </Row>
+                </FormItem>
+              </Form>
+            </div>
+          </transition>
+          <!--忘记密码-->
+          <transition name="fade">
+            <div class="forget-box" v-show="now==='forget'||now==='next'">
+              <Form ref="loginForm" :model="formLogin">
+
+                <div v-show="now==='forget'">
+                  <FormItem label="Telephone">
+                    <!--<Icon type="person"></Icon>-->
+                    <Input placeholder="请输入手机号"  :maxlength="11" type="text" v-model="forgetForm.username">
+                    <span slot="prepend" class="login-ico"> <Icon type="person"></Icon></span>
+                    </Input>
+                  </FormItem>
+                  <FormItem label="Code" >
+                    <Input placeholder="请输入验证码" type="password" v-model="forgetForm.authCode">
+                    <span slot="prepend" class="login-ico"><Icon type="outlet"></Icon></span>
+                    <span slot="append" class="code-btn">
+                    <Button  type="primary" @click="getCodingHandle" :loading="getCoding">验证码</Button>
+                  </span>
+                    </Input>
+                  </FormItem>
+                  <FormItem>
+                    <Button type="primary" long @click="togglePanel('next')">下一步</Button>
+                  </FormItem>
+                </div>
+                <transition name="slide-fade">
+                  <div v-show="now==='next'">
+                    <FormItem label="Password">
+                      <Input placeholder="请输入新密码" type="password" v-model="forgetForm.newPwd">
+                      <span slot="prepend" class="login-ico"><Icon type="ios-locked"></Icon></span>
+                      </Input>
+                    </FormItem>
+                    <FormItem>
+                      <Button type="primary" long>确认</Button>
+                    </FormItem>
+                  </div>
+                </transition>
+
+                <div >
+                  <Row>
+                    <Col span="12" offset="12" class-name="text-right">
+                      <Button type="text" @click="togglePanel('login')"><span class="back"><Icon type="chevron-left"></Icon> 返回登录</span></Button>
+                    </Col>
+                  </Row>
+                </div>
+              </Form>
+            </div>
+          </transition>
+
+          <div class="toggle-login" v-show="now==='login'||now==='register'">
+             <span ref="toLogin" class="active"  @click="togglePanel('login')">登录</span>
+             <span class="split">|</span>
+             <span ref="toRegister" @click="togglePanel('register')">注册</span>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login",
+        data:()=>{
+          //自定义验证手机号
+          const validateTel=(rule,value,callback)=>{
+            let telReg = /^1[3456789]\d{9}/;
+            if(!telReg.test(value)){
+              callback(new Error('请输入正确的手机号码'))
+            }else{
+              callback();
+            }
+          }
+          return {
+            formLogin:{
+              username:'',
+              password:'',
+              authCode:'',
+            },
+            ruleLogin:{
+              username:[
+                {required:true,message:'请输入手机号',trigger:'blur'},
+                {validator:validateTel,trigger:'blur'},
+              ],
+              password:[
+                {required:true,message:'请输入密码',trigger:'blur'},
+                {type:'string',min:6,message:'密码至少6位'}
+              ],
+              authCode:[
+                {required:true,message:'请输入验证码',trigger:'blur'},
+                {type:'number',min:4,message:'至少4位'}
+              ]
+            },
+            forgetForm:{
+              username:'',
+              authCode:'',
+              newPwd:'',
+            },
+            now:'login',
+            getCoding:false, //验证码获取中
+          }
+        },
+      methods:{
+        togglePanel(type){
+          switch(type){
+            case 'register':
+              this.$refs.toRegister.className="active";
+              this.$refs.toLogin.className="";
+              this.now='register';
+              break;
+            case 'login':
+              this.$refs.toRegister.className="";
+              this.$refs.toLogin.className="active";
+              this.now='login';
+              break;
+            case 'forget':
+              this.now='forget';
+              break;
+            case 'next':
+              this.now='next';
+              break;
+          }
+        },
+        getCodingHandle(){
+          //获取验证码
+          this.getCoding=true;
+          setTimeout(()=>{
+            this.getCoding=false;
+          },1000)
+        },
+        validateLogin(){
+          //验证登录
+          let tel = this.formLogin.username;
+          let password = this.formLogin.password;
+
+        }
+      }
+    }
+</script>
+
+<style scoped lang="scss">
+.login-page{
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: url('./bg.jpg') no-repeat center center;
+  background-size:100% 100%;
+  .login-container{
+    width: 567px;
+    height: 478px;
+    background: url('./login.png') no-repeat center center;
+    background-size:100% 100%;
+    position: absolute;
+    top: 15%;
+    right: 10%;
+    //transform:translate(0,-70%);
+    box-sizing: border-box;
+    padding: 18px;
+    display: flex;
+  }
+  .login-left{
+    vertical-align: top;
+    width: 246px;
+    padding-top:130px;
+    height: 100%;
+    img{
+      display: block;
+      margin: 0 auto;
+      width: 80%;
+    }
+    h4{
+      color: #fff;
+      font-size: 18px;
+      font-weight: bold;
+      text-align: center;
+      margin-top: 15px;
+      letter-spacing: 2px;
+    }
+  }
+  .login-right{
+    flex: 1;
+    /*width: 275px;*/
+    height: 100%;
+    box-sizing: border-box;
+    position: relative;
+    .login-box,.forget-box{
+      width: 100%;
+      padding: 15px;
+
+      position: absolute;
+      top: 50%;
+      transform:translate(0,-50%)
+    }
+    .toggle-login{
+      position: absolute;
+      bottom: 20px;
+      text-align: center;
+      font-size: 14px;
+      left: 0;
+      right: 0;
+      color: #ccc;
+      span{
+        padding: 0 10px;
+        user-select: none;
+        cursor: pointer;
+      }
+      .split{
+        cursor: default;
+      }
+      .active{
+        color: #039dff;
+      }
+    }
+  }
+  .back{
+    color: #ccc;
+    font-size: 14px;
+    transition:all .3s linear;
+    &:hover{
+      color: #039dff;
+    }
+  }
+}
+
+</style>
